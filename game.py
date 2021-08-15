08d0c2f87d9b160e96183dc63eb105fda74ace3f
LewisClifford <84357646+LewisClifford@users.noreply.github.com>
2021-08-15 05:32:29 +0100

    Final Release
    This is the Final Release for my BlackJack game. If you have found any bugs within the program, please contact me either by email or discord

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 # Card deck list
 
@@ -13,7 +14,7 @@ cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K]
 start = input("Would you like to play Blackjack?")
 if start == "Yes":
     name = input(str("What is your name?"))
-    counter = int(input("How many counters do you want to play? Max number of counters are 2500"))
+    counter = int(input("How many counters do you want? Max number of counters are 2500"))
     if counter > 2500:
         counter = int(input("Can't have more than 2500, Please enter 2500 or under"))
 else:
@@ -22,85 +23,106 @@ else:
 
 # Counter play
 
-counterplay = int(input("How many counters do you want to play? Min counters is 100"))
+while True:
+    counterplay = int(input("How many counters do you want to play? Min counters is 100"))
+    if counterplay < 100:
+        counterplay = int(input("Can't have less than 100, Re-type the counters you want to play!"))
 
-# User cards
+    # User cards
 
-ucard1 = random.choice(cards)
-ucard2 = random.choice(cards)
-utotal = ucard1 + ucard2
-print("Your first card is:", ucard1, "and your second card is:", ucard2, "with the total of:", utotal)
-if utotal == 21:
-    print("Blackjack!", name, "wins!")
-    counter = counter - counterplay
-# Computer cards
-
-dcards1 = random.choice(cards)
-dcards2 = random.choice(cards)
-dtotal = dcards1 + dcards2
-print("The dealer's first card is:", dcards1)
-
-# Asks the user if they want to Hit, Stand or Double
-
-uanswer = input(str("Would you like to Hit, Stand or Double?"))
-
-# Hit Play
-
-if uanswer == "Hit":
-    ucard3 = random.choice(cards)
-    utotal = utotal + ucard3
+    ucard1 = random.choice(cards)
+    ucard2 = random.choice(cards)
+    utotal = ucard1 + ucard2
+    print("Your first card is:", ucard1, "and your second card is:", ucard2, "with the total of:", utotal)
     if utotal == 21:
-      print(name, "wins with the total of",utotal," with the 3rd card",ucard3)
-      counter = counter - counterplay
-    elif utotal != 22:
-        print(utotal)
-        uanswer2 = input("Would you like to Hit, Stand or Double?")
-        if uanswer == "Hit":
-            ucard4 = random.choice(cards)
-            utotal = utotal + ucard4
-            if utotal == 21:
-                print(name, "wins with the total of",utotal,"! Their 4th card was",ucard4)
-                counter = counter - counterplay
+        print("Blackjack!", name, "wins!")
+        counter = counter - counterplay
+    # Computer cards
+
+    dcards1 = random.choice(cards)
+    dcards2 = random.choice(cards)
+    dtotal = dcards1 + dcards2
+    print("The dealer's first card is:", dcards1)
+
+    # Asks the user if they want to Hit, Stand or Double
+
+    uanswer = input(str("Would you like to Hit, Stand or Double?"))
+
+    # Hit Play
+
+    if uanswer == "Hit":
+        ucard3 = random.choice(cards)
+        utotal = utotal + ucard3
+        if utotal == 21:
+          print(name, "wins with the total of",utotal," with the 3rd card",ucard3)
+          counter = counter + (counterplay * 2)
+        elif utotal != 22:
+            print(utotal)
+            uanswer2 = input("Would you like to Hit, Stand or Double?")
+            if uanswer == "Hit":
+                ucard4 = random.choice(cards)
+                utotal = utotal + ucard4
+                if utotal == 21:
+                    print(name, "wins with the total of",utotal,"! Their 4th card was",ucard4)
+                    counter = counter + (counterplay * 2)
             elif uanswer2 == "Stand":
-                print("The dealer has",dcards1, "and their second card is",dcards2,"and the total of",dtotal)
-                if dtotal >= utotal + 1:
+                 print("The dealer has",dcards1, "and their second card is",dcards2,"and the total of",dtotal)
+                 if dtotal >= utotal + 1:
                     print("Dealer wins")
-                elif dtotal <= utotal - 1:
+                    counter = counter - counterplay
+                 elif dtotal <= utotal - 1:
                     print(name, "wins")
+                    counter = counter + (counterplay * 2)
+                 elif utotal >= 22:
+                    print("Bust")
+                    counter = counter - counterplay
             elif utotal >= 22:
                 print("Bust")
                 counter = counter - counterplay
-    elif utotal >= 22:
-        print("Bust")
-        counter = counter - counterplay
-    else:
-        exit()
-    # Stand Play
-
-elif uanswer == "Stand":
-    print("The dealer has", dtotal)
-
-# Double Play
-
-elif uanswer == "Double":
-    counterplay = counterplay * 2
-    ucard3 = random.choice(cards)
-    utotal = utotal + ucard3
-    if utotal == 21:
-        print(name, "wins with the total of",utotal,"with the third card",ucard3)
-        counter = counter - counterplay
-    elif utotal >= 22:
-        print("Dealer wins with the total of",dtotal)
-        counter = counter - counterplay
-    elif utotal != 21:
-        if dtotal <= utotal:
-            print(name, "wins with the total of", utotal)
-            counter = counter - counterplay
-        elif dtotal == utotal:
-            print("Draw!")
         else:
-            print("You got", utotal, "and dealer wins with",dtotal)
-            counter = counter - counterplay
+            exit()
+        # Stand Play
 
-# Prints counters left
-print("You have",counter,"left!")
+    elif uanswer == "Stand":
+        print("The dealer has", dtotal)
+        if utotal > dtotal:
+            print(name, "wins with" ,utotal)
+            counter = counter + (counterplay * 2)
+        elif dtotal > utotal:
+            print("Dealer wins with", dtotal)
+            counter = counter - counterplay
+    # Double Play
+
+    elif uanswer == "Double":
+        counterplay = counterplay * 2
+        ucard3 = random.choice(cards)
+        utotal = utotal + ucard3
+        if utotal == 21:
+            print(name, "wins with the total of",utotal,"with the third card",ucard3)
+            counter = counter + (counterplay * 2)
+        elif utotal >= 22:
+            print("Dealer wins with the total of",dtotal)
+            counter = counter - counterplay
+        elif utotal != 21:
+            if dtotal <= utotal:
+                print(name, "wins with the total of", utotal)
+                counter = counter + (counterplay * 2)
+            elif dtotal == utotal:
+                print("Draw!")
+            else:
+                print("You got", utotal, "and dealer wins with",dtotal)
+                counter = counter - counterplay
+
+    # Prints counters left
+    print("You have",counter,"left!")
+    TryAgain = input("Do you wanna play again?")
+    if TryAgain == "Yes":
+        if counter > 100:
+            print("Playing again!")
+        else:
+            print("Cannot play with less than 100 counters!")
+            sys.exit()
+    else:
+        print("Leaving Game")
+        exit()
+
